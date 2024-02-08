f3e243c808b5131c59b9fb8ff68811e493d8f9b6
Darian Nelson <darian.nelson@mscoding.org>
2024-02-08 14:58:29 -0600

    Added Input Row

--- src/components/InputRow.js
+++ src/components/InputRow.js
@@ -1,5 +1,25 @@
 import React from 'react'
 
-
+function InputRow() {
+    return(
+        <div className="col-sm-4">
+            <div className="row inputRow">
+                <div className="col-sm-12">
+                    <input type="text" name="Task" id="task" />
+                </div>
+                <div className="col-sm-4">
+                    <input type="text" name="TimeDue" id="timeDue" />
+                </div>
+                <div className="col-sm-7">
+                    <input type="text" name="Details" id="details" />
+                </div>
+                <div className="col-sm-1">
+                    {/* <input className="checkbox" type="checkbox" value={props.isDone}/> */}
+                </div>
+                
+            </div>
+        </div>
+    )
+}
 
 export default InputRow
--- src/components/ToDoRowList.js
+++ src/components/ToDoRowList.js
@@ -1,34 +1,13 @@
 import React from 'react'
 
-// const ToDoListData = [
-//     {   
-//         "task": "Clean Bathroom",
-//         "timeDue": "4:00 pm",
-//         "details": "Clean bathroom",
-//         "isDone": false
-//     }
-// ]
-function InputRow() {
-    return(
-        <div className="col-sm-4">
-            <div className="row inputRow">
-                <div className="col-sm-12">
-                    <input type="text" name="Task" id="task" />
-                </div>
-                <div className="col-sm-4">
-                    <input type="text" name="TimeDue" id="timeDue" />
-                </div>
-                <div className="col-sm-7">
-                    <input type="text" name="Details" id="details" />
-                </div>
-                <div className="col-sm-1">
-                    {/* <input className="checkbox" type="checkbox" value={props.isDone}/> */}
-                </div>
-                
-            </div>
-        </div>
-    )
-}
+const ToDoListData = [
+    {   
+        "task": "Clean Bathroom",
+        "timeDue": "4:00 pm",
+        "details": "Clean bathroom",
+        "isDone": false
+    }
+]
 
 function ToDoListItem(props) {
     console.log(props)
